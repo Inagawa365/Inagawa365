c6a1bec1bb3d0573bdb99c991dd5f3dc4e0c1eb6
Inagawa365 <kazuking-0720@ezweb.ne.jp>
2020-02-08 10:10:19 +0900

    basic

---
+++ basic/src/basic/task2.java
@@ -0,0 +1,12 @@
+package basic;
+
+public class task2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		System.out.println("hogehoge");
+        System.out.print("hogehoge2");
+	}
+
+}
