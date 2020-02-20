6c54df3aaa090dfa34186a23efb04ebaabbcfe94
Inagawa365 <kazuking-0720@ezweb.ne.jp>
2020-02-20 10:59:56 +0900

    課題提出(Java)-array

---
+++ array/src/array/task2.java
@@ -0,0 +1,29 @@
+package array;
+
+public class task2 {
+
+	public static void main(String[] args) {
+
+		int[] array  = new int[7]; //要素数
+
+		//7から降順
+		for(int i = array.length; i >= 1; i--){
+			System.out.print(i);
+
+			if(i < array.length + 1){
+				System.out.print(",");
+	        }
+	    }
+
+	     System.out.println(""); //改行を出力
+
+	    //1から昇順
+        for(int j = 1; j < array.length + 1; j++) {
+            System.out.print(j);
+
+            if(j < array.length){
+                System.out.print(",");
+            }
+        }
+   }
+}
---
+++ array/src/array/task3.java
@@ -0,0 +1,33 @@
+package array;
+
+import java.util.Random;
+
+public class task3 {
+
+	public static void main(String[] args) {
+
+		Random ransuu = new Random(); //乱数の設定
+		int[] n = new int[6]; //要素数
+		int Max = 0; //ありえない最大数で初期化
+        int Min = 10; //ありえない最小値で初期化
+
+		System.out.print("[");
+
+		for(int i = 0; i < n.length; i++){
+			n[i] = ransuu.nextInt(9) + 1;
+            System.out.print(n[i]);
+
+            if(i < n.length - 1){
+    			System.out.print(",");
+            }
+
+            if(Max < n[i]){Max = n[i];}
+            if(Min > n[i]){Min = n[i];}
+		}
+
+		System.out.print("]");
+		System.out.println(""); //改行を出力
+		System.out.println("最大値:" + Max);
+		System.out.println("最小値:" + Min);
+	}
+}
