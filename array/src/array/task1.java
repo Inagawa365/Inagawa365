2ae8ea05f71f683bf9b1275b01ae0d9696d80b2b
Inagawa365 <kazuking-0720@ezweb.ne.jp>
2020-03-10 22:38:52 +0900

    再提出 - 配列

--- array/src/array/task1.java
+++ array/src/array/task1.java
@@ -2,29 +2,28 @@ package array;
 
 public class task1 {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		int[] ranum = {4, 9, 19, 5, 8, 21, 42, 64, 73, 18, 2};
+        int[] ranum = {4, 9, 19, 5, 8, 21, 42, 64, 73, 18, 2};
+
         System.out.print("配列:[");
 
         //配列を一つずつ表示していく
-		for(int i = 0; i < ranum.length; i++){
-		    System.out.print(ranum[i]);
-
-		    //","を最後の整数の後ろには表示しない
-		    if(i < ranum.length - 1){
-			    System.out.print("," );
+        for(int i = 0; i < ranum.length; i++){
+            System.out.print(ranum[i]);
+            if(i < ranum.length - 1){
+                System.out.print("," ); //","を最後の整数の後ろには表示しない
             }
-	    }
-		System.out.println("]");
+        }
+        System.out.println("]");
 
-		//拡張for文で配列の整数を加算
-	    int sum = 0;
+        //拡張for文で配列の整数を加算
+        int sum = 0;
 
-	    for(int data: ranum){
-	    	sum += data;
-	    }
-	    System.out.print("合計:" + sum);
-  }
+        for(int data: ranum){
+            sum += data;
+        }
+        System.out.print("合計:" + sum);
+    }
 }
 
--- array/src/array/task3.java
+++ array/src/array/task3.java
@@ -4,30 +4,29 @@ import java.util.Random;
 
 public class task3 {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		Random ransuu = new Random(); //乱数の設定
-		int[] n = new int[6]; //要素数
-		int Max = 0; //ありえない最大数で初期化
-        int Min = 10; //ありえない最小値で初期化
+        Random ransuu = new Random(); //乱数の設定
+        int[] n = new int[6]; //要素数
+        int Max = 0; //ありえない最大数で初期化
+        int Min = 0; //ありえない最小値で初期化
 
-		System.out.print("[");
+        System.out.print("[");
 
-		for(int i = 0; i < n.length; i++){
-			n[i] = ransuu.nextInt(9) + 1;
+        for(int i = 0; i < n.length; i++){
+            n[i] = ransuu.nextInt(9) + 1; // "0～9"を「 +1 」して"1～10"にする
             System.out.print(n[i]);
-
             if(i < n.length - 1){
-    			System.out.print(",");
+                System.out.print(",");
             }
-
             if(Max < n[i]){Max = n[i];}
             if(Min > n[i]){Min = n[i];}
-		}
+        }
+        System.out.print("]");
+        System.out.println(""); //改行を出力
 
-		System.out.print("]");
-		System.out.println(""); //改行を出力
-		System.out.println("最大値:" + Max);
-		System.out.println("最小値:" + Min);
-	}
+        //乱数で出た値の最大値, 最小値を出力
+        System.out.println("最大値:" + Max);
+        System.out.println("最小値:" + Min);
+    }
 }
