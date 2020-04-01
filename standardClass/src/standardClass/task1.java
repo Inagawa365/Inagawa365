9e16ce3ae9f6875a996c17d74eb1e37c9cc7600d
Inagawa365 <kazuking-0720@ezweb.ne.jp>
2020-04-01 10:01:23 +0900

    再提出 - 標準クラス

--- standardClass/src/standardClass/task1.java
+++ standardClass/src/standardClass/task1.java
@@ -5,7 +5,7 @@ public class task1 {
     public static void main(String[] args) {
 
         String str1 = " tech-compass ";
-        String str2 = str1.toUpperCase().trim(); //文字列を大文字に変換し、前後空白を削除する
+        String str2 = str1.toUpperCase().trim(); //文字列を大文字に変換し、前後空白を削除する。
 
         System.out.println("変換前:" + str1);
         System.out.println("変換後:" + str2);
